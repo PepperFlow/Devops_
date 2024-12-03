3f335319d0529430c77ecdc4d9ba90bca4649750
Wuttichai Chueaklang <pete@mbpsomtuttichai.home>
2024-12-03 12:48:33 +0100

    Removed test.py and updated test_main.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from main import app
-
-def test_home():
-    with app.test_client() as client:
-        response = client.get('/')
-        assert response.status_code == 200
-        assert "Väder i" in response.data.decode('utf-8')  
